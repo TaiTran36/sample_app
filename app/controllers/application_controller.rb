87cb5a63a0a13332f66028f9d1df4e6ccfe58b42
Tai Tran <tranthanhtai257@gmail.com>
2018-07-12 13:35:17 +0700

    Mostly static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  #   def set_locale
+  #     locale = params[:locale].to_s.strip.to_sym
+  #     if I18n.locale = I18n.available_locales.include
+  #     elsif locale
+  #       I18n.default_locale
+  #     end
+  #   end
+
+  before_action :st_locale
+  def st_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
