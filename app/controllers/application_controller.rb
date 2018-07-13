c4ce0a8bdd6bcbdb2e8b2fe85d4e6bc5e2227968
Tai Tran <tranthanhtai257@gmail.com>
2018-07-13 14:57:22 +0700

    Sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,8 @@
 class ApplicationController < ActionController::Base
-  #   def set_locale
-  #     locale = params[:locale].to_s.strip.to_sym
-  #     if I18n.locale = I18n.available_locales.include
-  #     elsif locale
-  #       I18n.default_locale
-  #     end
-  #   end
+  before_action :set_locale
 
-  before_action :st_locale
-  def st_locale
+  private
+  def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 end
