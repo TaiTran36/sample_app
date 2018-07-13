a7e58738bd2f8d5ba5629da31fc76c4241e86055
Tai Tran <tranthanhtai257@gmail.com>
2018-07-13 14:57:22 +0700

    Basic login

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  before_save :email_downcase
+
+  validates :name, presence: true,
+    length: {maximum: Settings.name_max_length}
+
+  validates :email, presence: true,
+    length: {maximum: Settings.mail_max_length},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+
+  validates :password, presence: true,
+    length: {minimum: Settings.password_min_length}
+
+  has_secure_password
+
+  def self.digest string
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  private
+  def email_downcase
+    self.email = email.downcase
+  end
+end
