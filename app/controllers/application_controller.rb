902106acc9e6ee34b861a7f1410f6a6b8f3bc2eb
Tai Tran <tranthanhtai257@gmail.com>
2018-07-13 14:57:22 +0700

    Basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < ActionController::Base
-  #   def set_locale
-  #     locale = params[:locale].to_s.strip.to_sym
-  #     if I18n.locale = I18n.available_locales.include
-  #     elsif locale
-  #       I18n.default_locale
-  #     end
-  #   end
+  protect_from_forgery with: :exception
+  include SessionsHelper
 
-  before_action :st_locale
-  def st_locale
+  before_action :set_locale
+
+  private
+  def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 end
