32e34772c84bd04015c73c22f7a8255ef335bcda
Tai Tran <tranthanhtai257@gmail.com>
2018-07-18 15:57:13 +0700

    Advenced Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,27 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate params[:session][:password]
-      log_in user
-      redirect_to user
+    @user = User.find_by email: params[:session][:email].downcase
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      check_user
+      redirect_to @user
     else
       flash.now[:danger] = t ".invalid"
       render :new
     end
   end
 
+  def check_user
+    if params[:session][:remember_me] == "1"
+      remember(@user)
+    else
+      forget(@user)
+    end
+  end
+
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
